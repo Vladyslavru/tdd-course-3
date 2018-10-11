24ef5c228d007bae9bb67a83c63102d4fd3eb479
Vladislavru <vladislav.babushkin97@gmail.com>
2018-10-11 15:30:57 +0300

    green

--- tdd_intro/homework/02_ternary_numbers/test.cpp
+++ tdd_intro/homework/02_ternary_numbers/test.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-
+#include <string>
 /*
 Convert a ternary number, represented as a string (e.g. '102012'), to its decimal equivalent using first principles.
 
@@ -17,7 +17,10 @@ The last place in a ternary number is the 1's place. The second to last is the 3
 If your language provides a method in the standard library to perform the conversion, pretend it doesn't exist and implement it yourself.
 */
 
-
+std::string ConvertToTernary(std::string ternaryNumber)
+{
+    return "1";
+}
 
 TEST(TernaryConvertion, Convert1)
 {
