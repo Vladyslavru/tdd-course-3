38adb5e7ae2e6dcbafa405848feb6595e4ef715b
Vladislavru <vladislav.babushkin97@gmail.com>
2018-10-29 01:04:24 +0200

    red

--- tdd_intro/homework/04_weather_client/test.cpp
+++ tdd_intro/homework/04_weather_client/test.cpp
@@ -175,3 +175,25 @@ TEST(Weather, ParseCorrectDataSet)
 
     EXPECT_EQ(weatherSet, GetWeatherSet(serv, "31.08.2018"));
 }
+
+// ------------------------------
+
+short GetAverageTemp(const WeatherSet& set)
+{
+    return 0;
+}
+
+TEST(Weather, GetAverageTemperature)
+{
+    FakeServer serv;
+    EXPECT_CALL(serv, GetWeather("31.08.2018;03:00")).WillOnce(Return("20;181;5.1"));
+    EXPECT_CALL(serv, GetWeather("31.08.2018;09:00")).WillOnce(Return("23;204;4.9"));
+    EXPECT_CALL(serv, GetWeather("31.08.2018;15:00")).WillOnce(Return("33;193;4.3"));
+    EXPECT_CALL(serv, GetWeather("31.08.2018;21:00")).WillOnce(Return("26;179;4.5"));
+
+    WeatherSet weatherSet = {{20, 181, 5.1}, {23, 204, 4.9}, {33, 193, 4.3}, {26, 179, 4.5}};
+
+    ASSERT_EQ(weatherSet, GetWeatherSet(serv, "31.08.2018"));
+
+    EXPECT_EQ(GetAverageTemp(weatherSet), 25);
+}
